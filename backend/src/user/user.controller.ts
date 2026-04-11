89cb500db9807f651e1ff84f8e99b2b4ff7defc7
Vinicius Bonfim <viniciusbonfim.f@gmail.com>
2026-04-11 13:02:21 -0300

    feat: create DTO using pipe

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from '@/user/dtos/createUser.dto';
 import { UserService } from '@/user/user.service';
-import { UserEntity } from '@/user/entity/user.entity';
+import { ReturnUserDto } from '@/user/dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Get()
-  async getAllUsers(): Promise<UserEntity[]> {
-    return this.userService.getAllUser();
-  }
+  async getAllUsers(): Promise<ReturnUserDto[]> {
+    const users = await this.userService.getAllUser();
 
+    const userDto = users.map((users) => new ReturnUserDto(users));
+
+    return userDto;
+  }
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
