ece60443c4c553cdeeb2fc97318c277a6710c943
Vinicius Bonfim <viniciusbonfim.f@gmail.com>
2026-04-13 17:49:35 -0300

    feat: insert DTO's to city and state

--- backend/src/address/dto/returnAddress.fto.ts
+++ backend/src/address/dto/returnAddress.fto.ts
@@ -1,14 +1,16 @@
 import { AddressEntity } from '@/address/entity/address.entity';
+import { ReturnCityDto } from '@/city/dto/returnCity.dto';
 
 export class ReturnAddressDto {
   complement!: string;
   numberAddress!: number;
   cep!: string;
-  cityId?: any;
+  city?: ReturnCityDto;
 
   constructor(adress: AddressEntity) {
     this.complement = adress.complement;
     this.numberAddress = adress.numberAddress;
     this.cep = adress.cep;
+    this.city = adress.city ? new ReturnCityDto(adress.city) : undefined;
   }
 }
